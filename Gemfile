f512d1894c8dc0d3131cd50f2e87fb05cbbc425d
Matthew Ng <matthew.yan.ng@gmail.com>
2016-01-12 12:57:27 +0100

    forked middleman for github user pages

--- Gemfile
+++ Gemfile
@@ -4,11 +4,16 @@ source 'https://rubygems.org'
 
 gem "middleman", "~>3.3.12"
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-
 # For faster file watcher updates on Windows:
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
+
+gem "compass"
+
+gem "redcarpet"
+
+gem "middleman-syntax"
+
+gem "middleman-deploy", "~> 1.0"
--- config.rb
+++ config.rb
@@ -1,72 +1,41 @@
-###
-# Compass
-###
+page "cv.html", :layout => false
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+activate :automatic_image_sizes
 
-###
-# Page options, layouts, aliases and proxies
-###
+set :layouts_dir, '_layouts'
+set :partials_dir, '_partials'
 
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+set :css_dir, 'assets/css'
 
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+set :js_dir, 'assets/js'
 
-###
-# Helpers
-###
+set :images_dir, 'assets/img'
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
 
-# Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+activate :syntax, :line_numbers => false
+activate :directory_indexes
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+ignore '*.md'
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
+helpers do
+  def markdown(text)
+    Tilt['markdown'].new { text }.render
+end
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :relative_assets
+end
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+activate :deploy do |deploy|
+ deploy.method = :git
+ deploy.build_before = true
+ deploy.branch   = "master"
+ deploy.remote   = "https://github.com/matthew-ng/matthew-ng.github.io.git"
 end
